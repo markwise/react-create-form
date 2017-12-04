1c2eab772c3f3f5adfd582d2cb04404a80345c72
Mark Wise <m2r5.w4s3@gmail.com>
2017-12-04 12:46:44 -0600

    updating/adding more unit tests for createForm

--- src/createForm.spec.js
+++ src/createForm.spec.js
@@ -3,12 +3,11 @@ import {mount, shallow} from 'enzyme'
 import {createForm} from './createForm'
 import {rules} from './rules'
 
-const createWrapper = fields => {
+const createShallowWrapper = fields => {
   let Form = () => <form></form>
   let TestForm = createForm(Form, fields)
   return shallow(<TestForm />)
 }
-
 
 describe('hoc', () => {
   it('should have prototype', () => {
@@ -23,18 +22,18 @@ describe('hoc', () => {
   })
 
   it('should init default values', () => {
-    let wrapper = createWrapper({name: {}})
+    let wrapper = createShallowWrapper({name: {}})
     let {name} = wrapper.state()
     expect(name.value).toBe('')
-    // label, rules and clean are set for fields that will validate
-    expect(name.label).toBe(void 0)
-    expect(name.rules).toBe(void 0)
-    expect(name.clean).toBe(void 0)
+    // label, rules and clean should only be set for fields that will validate
+    expect(name).not.toHaveProperty('label')
+    expect(name).not.toHaveProperty('rules')
+    expect(name).not.toHaveProperty('clean')
   })
 
   it('should init default values for fields that will validate', () => {
     let rules = [() => {}]
-    let wrapper = createWrapper({name: {rules}})
+    let wrapper = createShallowWrapper({name: {rules}})
     let {name} = wrapper.state()
     expect(name.value).toBe('')
     expect(name.label).toBe('')
@@ -43,17 +42,18 @@ describe('hoc', () => {
   })
 
   it('should not init default values for fields that will validate', () => {
-    // rules must have at least one validation function
-    let wrapper = createWrapper({name: {rules: []}})
+    let wrapper = createShallowWrapper({name: {rules: []}})
     let {name} = wrapper.state()
     expect(name.value).toBe('')
-    expect(name.label).toBe(void 0)
-    expect(name.rules).toBe(void 0)
-    expect(name.clean).toBe(void 0)
+    // rules must have at least one validation function for label, rules and
+    // clean to be set
+    expect(name).not.toHaveProperty('label')
+    expect(name).not.toHaveProperty('rules')
+    expect(name).not.toHaveProperty('clean')
   })
 
   it('should init values', () => {
-    let wrapper = createWrapper({name: {value: 'Mark Wise'}})
+    let wrapper = createShallowWrapper({name: {value: 'Mark Wise'}})
     let {name} = wrapper.state()
     expect(name.value).toBe('Mark Wise')
   })
@@ -61,7 +61,7 @@ describe('hoc', () => {
   it('should init values for fields that will validate', () => {
     let rules = [() => {}]
     let fields = {name: {value: 'Mark Wise', label: 'Name', rules}}
-    let wrapper = createWrapper(fields)
+    let wrapper = createShallowWrapper(fields)
     let {name} = wrapper.state()
     expect(name.value).toBe('Mark Wise')
     expect(name.label).toBe('Name')
@@ -73,7 +73,7 @@ describe('hoc', () => {
 
 describe('wrapped component', () => {
   it('should have props', () => {
-    let wrapper = createWrapper({})
+    let wrapper = createShallowWrapper({})
     let props = wrapper.props()
     expect(props.form).toEqual(expect.any(Object))
     expect(props.onChange).toEqual(expect.any(Function))
@@ -84,18 +84,18 @@ describe('wrapped component', () => {
   })
 
   it('props.form should have shape', () => {
-    let wrapper = createWrapper({})
+    let wrapper = createShallowWrapper({})
     let props = wrapper.props()
     let {form} = props
     let {errors, willSubmit} = form
     expect(errors).toEqual(expect.any(Array))
     expect(errors.length).toBe(0)
-    // Will be true when there are no fields to validate
+    // Always true when there are no fields to validate
     expect(willSubmit).toBe(true)
   })
 
   it('props.form should have shape when fields will validate', () => {
-    let wrapper = createWrapper({name: {rules: [() => {}]}})
+    let wrapper = createShallowWrapper({name: {rules: [() => {}]}})
     let props = wrapper.props()
     let {form} = props
     let {errors, willSubmit} = form
@@ -106,17 +106,17 @@ describe('wrapped component', () => {
   })
 
   it('props.form.<field> should have shape', () => {
-    let wrapper = createWrapper({name: {}})
+    let wrapper = createShallowWrapper({name: {}})
     let props = wrapper.props()
-    let {value, errors, error} = props.form.name
-    expect(value).toBe('')
+    let {name} = props.form
+    expect(name.value).toBe('')
     // errors and error exist if the field will validate
-    expect(errors).toBe(void 0)
-    expect(error).toBe(void 0)
+    expect(name).not.toHaveProperty('errors')
+    expect(name).not.toHaveProperty('error')
   })
 
   it('props.form.<field> should have shape when field will validate', () => {
-    let wrapper = createWrapper({name: {rules: [() => {}]}})
+    let wrapper = createShallowWrapper({name: {rules: [() => {}]}})
     let props = wrapper.props()
     let {value, errors, error} = props.form.name
     expect(value).toBe('')
@@ -129,12 +129,14 @@ describe('wrapped component', () => {
 
 describe('wrapped component props.onChange', () => {
   it('should manage select-single, textarea, and input types', () => {
-    let wrapper = createWrapper({name: {}})
+    let wrapper = createShallowWrapper({name: {}})
     expect(wrapper.state().name.value).toBe('')
     expect(wrapper.props().form.name.value).toBe('')
 
     wrapper.props().onChange({
       currentTarget: {
+        // The handleChange event handles all types the same except for
+        // select-multiple, checkbox and file, which have their own tests.
         type: 'text',
         name: 'name',
         value: 'Mark Wise'
@@ -147,9 +149,9 @@ describe('wrapped component props.onChange', () => {
   })
 
   it('should manage select-multiple types', () => {
-    let wrapper = createWrapper({colors: {value: []}})
-    expect(wrapper.state().colors.value).toEqual([])
-    expect(wrapper.props().form.colors.value).toEqual([])
+    let wrapper = createShallowWrapper({colors: {value: []}})
+    expect(wrapper.state().colors.value).toEqual(expect.any(Array))
+    expect(wrapper.props().form.colors.value).toEqual(expect.any(Array))
 
     wrapper.props().onChange({
       currentTarget: {
@@ -161,8 +163,9 @@ describe('wrapped component props.onChange', () => {
     })
 
     wrapper.update()
-    expect(wrapper.state().colors.value).toEqual(['RED', 'GREEN', 'BLUE'])
-    expect(wrapper.props().form.colors.value).toEqual(['RED', 'GREEN', 'BLUE'])
+    let value = ['RED', 'GREEN', 'BLUE']
+    expect(wrapper.state().colors.value).toEqual(value)
+    expect(wrapper.props().form.colors.value).toEqual(value)
 
     wrapper.props().onChange({
       currentTarget: {
@@ -174,12 +177,13 @@ describe('wrapped component props.onChange', () => {
     })
 
     wrapper.update()
-    expect(wrapper.state().colors.value).toEqual(['GREEN'])
-    expect(wrapper.props().form.colors.value).toEqual(['GREEN'])
+    value = ['GREEN']
+    expect(wrapper.state().colors.value).toEqual(value)
+    expect(wrapper.props().form.colors.value).toEqual(value)
   })
 
   it('should manage checkbox types', () => {
-    let wrapper = createWrapper({yes: {}})
+    let wrapper = createShallowWrapper({yes: {}})
     expect(wrapper.state().yes.value).toBe('')
     expect(wrapper.props().form.yes.value).toBe('')
 
@@ -211,30 +215,31 @@ describe('wrapped component props.onChange', () => {
   })
 
   it('should manage file types', () => {
-    let wrapper = createWrapper({resume: {}})
-    let resume = wrapper.state().resume
-    expect(resume.files).toBe(void 0)
+    let wrapper = createShallowWrapper({resume: {}})
+    let {resume} = wrapper.state()
     expect(resume.value).toBe('')
+    expect(resume).not.toHaveProperty('files')
     expect(wrapper.props().form.resume.value).toBe('')
 
+    let value = 'C:\\fakepath\\Foo Bar.docx'
     wrapper.props().onChange({
       currentTarget: {
         type: 'file',
         name: 'resume',
-        value: 'C:\\fakepath\\Foo Bar.docx',
+        value,
         files: []
       }
     })
 
     wrapper.update()
     resume = wrapper.state().resume
+    expect(resume.value).toBe(value)
     expect(resume.files).toEqual(expect.any(Array))
-    expect(resume.value).toBe('C:\\fakepath\\Foo Bar.docx')
-    expect(wrapper.props().form.resume.value).toBe('C:\\fakepath\\Foo Bar.docx')
+    expect(wrapper.props().form.resume.value).toBe(value)
   })
 
   it('should manage validation lifecycle', () => {
-    let wrapper = createWrapper({name: {rules: [rules.required()()]}})
+    let wrapper = createShallowWrapper({name: {rules: [rules.required()()]}})
     let form = wrapper.props().form
     let name = form.name
     expect(wrapper.state().name.clean).toBe(true)
@@ -302,3 +307,100 @@ describe('wrapped component props.onChange', () => {
     expect(name.error.length).toBe(0)
   })
 })
+
+
+describe('wrapped component props.onReset', () => {
+  it('should reset state', () => {
+    let wrapper = createShallowWrapper({
+      firstName: {rules: [() => {}]},
+      lastName: {rules: [() => {}]}
+    })
+
+    let initialState = {
+      firstName: {value: '', clean: true},
+      lastName: {value: '', clean: true}
+    }
+
+    expect(wrapper.state()).toMatchObject(initialState)
+
+    wrapper.props().onChange({
+      currentTarget: {
+        type: 'text',
+        name: 'firstName',
+        value: 'Mark'
+      }
+    })
+
+    wrapper.props().onChange({
+      currentTarget: {
+        type: 'text',
+        name: 'lastName',
+        value: 'Wise'
+      }
+    })
+
+    expect(wrapper.state()).toMatchObject({
+      firstName: {value: 'Mark', clean: false},
+      lastName: {value: 'Wise', clean: false}
+    })
+
+    wrapper.props().onReset({preventDefault(){}})
+    expect(wrapper.state()).toMatchObject(initialState)
+  })
+})
+
+
+describe('wrapped component props.validate', () => {
+  it('should validate fields', () => {
+    let wrapper = createShallowWrapper({
+      firstName: {rules: [rules.required()()]},
+      lastName: {rules: [rules.required()()]}
+    })
+
+    expect(wrapper.state()).toMatchObject({
+      firstName: {clean: true},
+      lastName: {clean: true}
+    })
+
+    let props = wrapper.props()
+    let form = props.form
+    expect(form).toMatchObject({
+      errors: [],
+      firstName: {errors: [], error: ''},
+      lastName: {errors: [], error: ''}
+    })
+
+    props.validate(form).catch(() => {})
+    wrapper.update()
+
+    expect(wrapper.state()).toMatchObject({
+      firstName: {clean: false},
+      lastName: {clean: false}
+    })
+
+    form = wrapper.props().form
+    expect(form.errors.length).toBe(2)
+    let {firstName} = form
+    expect(firstName.errors.length).toBe(1)
+    expect(firstName.error.length).toBeGreaterThan(0)
+    let {lastName} = form
+    expect(lastName.errors.length).toBe(1)
+    expect(lastName.error.length).toBeGreaterThan(0)
+  })
+
+  it('should resolve promise', done => {
+    let wrapper = createShallowWrapper({})
+    wrapper.props().validate({willSubmit: true}).then(() => done())
+  })
+
+  it('should reject promise', done => {
+    let wrapper = createShallowWrapper({})
+    wrapper.props().validate({willSubmit: false}).catch(() => done())
+  })
+})
+
+
+describe('wrapped component props.getFormData', () => {})
+
+
+describe('wrapped component props.getFormDataAsJSON', () => {})
