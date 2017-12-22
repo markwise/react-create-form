f4eb9619b71c5737917d174b87749f98172e1fd0
Mark Wise <m2r5.w4s3@gmail.com>
2017-12-21 20:26:02 -0600

    Closes #14: Provide props to update a form's state from an external source

--- src/createForm.js
+++ src/createForm.js
@@ -1,5 +1,39 @@
 import React, {Component} from 'react'
+import PropTypes from 'prop-types'
 import {validateForm} from './validator'
+import compareArrays from './compareArrays'
+
+/**
+ * Shallow compares field values to determine if the component should update.
+ *
+ * @param {Object} nextFields
+ *    The nextProps fields property.
+ *
+ * @param {Object} prevFields
+ *    The current props fields property.
+ *
+ * @param {Object} state
+ *    The current state.
+ *
+ * @returns {Boolean}
+ *    Returns true if the form should update, otherwise, false.
+ */
+const shouldUpdate = (nextFields = {}, prevFields = {}, state) => {
+  let names = Object.keys(state)
+
+  return Object.entries(nextFields).some(([name, nextValue]) => {
+    // Skip if there is no field definition entry for name
+    if (!names.includes(name)) return false
+    let prevValue = prevFields[name]
+
+    // Compare select-multiple type values
+    if (Array.isArray(nextValue) && Array.isArray(prevValue)) {
+      return !compareArrays(nextValue, prevValue)
+    }
+
+    return nextValue !== prevValue
+  })
+}
 
 
 /**
@@ -70,108 +104,79 @@ export const createForm = (WrappedComponent, fields) => (
   class ReactCreateForm extends Component {
     static displayName = getDisplayName(WrappedComponent)
 
+    static defaultProps = {
+      id: null,
+      fields: {}
+    }
+
+    static propTypes = {
+      id: PropTypes.oneOfType([
+        PropTypes.object,
+        PropTypes.number,
+        PropTypes.string
+      ]),
+      fields: PropTypes.object
+    }
+
     constructor(props) {
       super(props)
       this.initialState = createInitialState(fields)
       this.state = this.initialState
-      this.handleChange = this.handleChange.bind(this)
-      this.handleReset = this.handleReset.bind(this)
-      this.validate = this.validate.bind(this)
-      this.getFormData = this.getFormData.bind(this)
+      this.update = this.update.bind(this)
+      this.reset = this.reset.bind(this)
       this.getFormDataAsJSON = this.getFormDataAsJSON.bind(this)
+      this.getFormData = this.getFormData.bind(this)
+      this.validate = this.validate.bind(this)
+      this.handleReset = this.handleReset.bind(this)
+      this.handleChange = this.handleChange.bind(this)
     }
 
+    componentDidMount() {
+      let {fields} = this.props
+      if (Object.keys(fields).length) this.update(fields)
+    }
 
-    /**
-     * Handles onChange for all html input types, select and textarea.
-     */
-    handleChange(event) {
-      let target = event.currentTarget
-      let {name, type, value} = target
-      let field = {...this.state[name], value, clean: false}
+    componentWillReceiveProps(nextProps) {
+      let {fields} = nextProps
 
-      if (type === 'select-multiple') {
-        field = {...field, value: [...target.selectedOptions].map(option => option.value)}
+      if (!Object.keys(fields).length) {
+        this.reset()
       } else
-      if (type === 'checkbox' && !target.checked) {
-        field = {...field, value: ''}
-      } else
-      if (type === 'file') {
-        field = {...field, files: target.files}
+      if (shouldUpdate(fields, this.props.fields, this.state)) {
+        this.update(fields)
       }
-
-      this.setState({[name]: field})
     }
 
 
     /**
-     * Resets field values to the initial state defined in the field
-     * definition object. This includes resetting fields that have been
-     * validated to an indeterminate state by setting clean to true.
+     * Updates field state values. If a field doesn't have a definition entry,
+     * it is ignored.
+     *
+     * @param {Object} fields
+     *    An object of field name/value pairs.
      */
-    handleReset(event) {
-      event.preventDefault()
+    update(fields) {
+      let names = Object.keys(this.state)
+
+      this.setState(prevState =>
+        Object.entries(fields).reduce((fields, [name, value]) => {
+          // Skip if there is no field definition entry for name
+          if (!names.includes(name)) return fields
+          let field = prevState[name]
+          let clean = field.clean && value === ''
+          return {...fields, [name]: {...field, value, clean}}
+        }, {})
+      )
+    }
+
+
+    /**
+     * Resets field values to the initial state defined in the field definition
+     * object. This includes resetting fields that have been validated to an
+     * indeterminate state by setting clean to true.
+     */
+    reset() {
       this.setState(this.initialState)
-    }
-
-
-    /**
-     * Technically this method does not validate, but forces all fields to be
-     * eligible for validation on the next render by setting each field's clean
-     * property to false. This means a field is not in an indeterminate state.
-     *
-     * @param {Object} form
-     *    A validated form object. This is passed internally.
-     *
-     * @returns {Promise}
-     *    A resolved promise if the form is valid, otherwise, a rejected promise.
-     */
-    validate(form) {
-      if (form.willSubmit) {
-        return Promise.resolve()
-      } else {
-        let fields = Object.entries(this.state)
-        let clean = fields.some(([name, field]) => field.clean)
-
-        // This will only run one time
-        if (clean) {
-          fields.forEach(([name, field]) => {
-            if (field.clean) {
-              this.setState({[name]: {...field, clean: false}})
-            }
-          })
-        }
-
-        return Promise.reject()
-      }
-    }
-
-
-    /**
-     * Returns a Promise that resolves field values as FormData. This method
-     * should be used when transmitting multipart/form-data (file uploads).
-     *
-     * @param {Array} [blacklist]
-     *    An optional list of fields to exclude from the resulting FormData.
-     *
-     * @returns {Promise}
-     *    A Promise that resolves it's value to FormData.
-     */
-    getFormData(blacklist) {
-      let fields = filterForm(this.state, blacklist)
-      let formData = new FormData()
-
-      fields.forEach(([name, {value, files}]) => {
-        if (files) {
-          ;[...files].forEach(file => formData.append(`${name}[]`, file))
-        } else {
-          formData.append(name, value)
-        }
-      })
-
-      // A promise is returned here for consistency with getFormDataAsJSON
-      // so they can be used interchangeably.
-      return Promise.resolve(formData)
     }
 
 
@@ -234,11 +239,105 @@ export const createForm = (WrappedComponent, fields) => (
     }
 
 
+    /**
+     * Returns a Promise that resolves field values as FormData. This method
+     * should be used when transmitting multipart/form-data (file uploads).
+     *
+     * @param {Array} [blacklist]
+     *    An optional list of fields to exclude from the resulting FormData.
+     *
+     * @returns {Promise}
+     *    A Promise that resolves it's value to FormData.
+     */
+    getFormData(blacklist) {
+      let fields = filterForm(this.state, blacklist)
+      let formData = new FormData()
+
+      fields.forEach(([name, {value, files}]) => {
+        if (files) {
+          ;[...files].forEach(file => formData.append(`${name}[]`, file))
+        } else {
+          formData.append(name, value)
+        }
+      })
+
+      // A promise is returned here for consistency with getFormDataAsJSON
+      // so they can be used interchangeably.
+      return Promise.resolve(formData)
+    }
+
+
+    /**
+     * Technically this method does not validate, but forces all fields to be
+     * eligible for validation on the next render by setting each field's clean
+     * property to false. This means a field is not in an indeterminate state.
+     *
+     * @param {Object} form
+     *    A validated form object. This is passed internally.
+     *
+     * @returns {Promise}
+     *    A resolved promise if the form is valid, otherwise, a rejected promise.
+     */
+    validate(form) {
+      if (form.willSubmit) {
+        return Promise.resolve()
+      } else {
+        let fields = Object.entries(this.state)
+        let clean = fields.some(([name, field]) => field.clean)
+
+        // This will only run one time
+        if (clean) {
+          fields.forEach(([name, field]) => {
+            if (field.clean) {
+              this.setState({[name]: {...field, clean: false}})
+            }
+          })
+        }
+
+        return Promise.reject()
+      }
+    }
+
+
+    /**
+     * Form onReset handler
+     */
+    handleReset(event) {
+      event.preventDefault()
+      this.reset()
+    }
+
+
+    /**
+     * Handles onChange for all html form element types.
+     */
+    handleChange(event) {
+      let target = event.currentTarget
+      let {name, type, value} = target
+      let field = {...this.state[name], value, clean: false}
+
+      if (type === 'select-multiple') {
+        field = {...field, value: [...target.selectedOptions].map(option => option.value)}
+      } else
+      if (type === 'checkbox' && !target.checked) {
+        field = {...field, value: ''}
+      } else
+      if (type === 'file') {
+        field = {...field, files: target.files}
+      }
+
+      this.setState({[name]: field})
+    }
+
+
     render() {
+      let {fields, ...props} = this.props
       let form = validateForm(this.state)
+
       return (
         <WrappedComponent
-          {...this.props}
+          {...props}
+          id={props.id || fields.id || null}
           form={form}
           onChange={this.handleChange}
           onReset={this.handleReset}
